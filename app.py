39e95cc7fe6ae2885a81fd4edc0a8e12a5eb872a
getResult <130590140+mrkosvl06@users.noreply.github.com>
2024-01-11 09:52:47 +0300

    Update app.py
    I was doing the tutorial and stumbled upon an error when creating database. These 2 lines will fix this error.

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
